2aec9c78a916127e1a73b62e27a4d4c95219ea0c
onimaskesi <53870740+onimaskesi@users.noreply.github.com>
2021-02-26 01:39:06 +0300

    Initial commit

---
+++ app/src/main/java/com/onimaskesi/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.onimaskesi.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onimaskesi.mvvmsampleapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
